320adbf7c562b4e1763a82952c015615ba225613
Artem Kravchenko <atyoma.kravchenko@gmail.com>
2021-12-22 00:53:40 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const refs = {
+  start: document.querySelector('button[data-start]'),
+  stop: document.querySelector('button[data-stop]'),
+  background: document.querySelector('body'),
+};
+const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 
+function getRandomHexColor() {
+  return randomColor;
+}
+
+console.log(randomColor);
+
+refs.start.addEventListener('click', onClickStart);
+// refs.stop.addEventListener('click', onClickStop);
+
+function onClickStart(e) {
+  refs.background.style.backgroundColor = randomColor;
+}
+console.log(refs.background);
+// function onClickStop(){}
